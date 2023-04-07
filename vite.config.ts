ebde3697e837267988a04a55137a8c43b14efe1c
piny4man <development@pinya.dev>
2023-04-07 12:18:37 +0200

    Fix production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: {}
-  },
   plugins: [
     react(),
     checker({
